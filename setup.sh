27a6223cfc5e3d601b1306f89f5031f641a81a98
Andy DuFrene <andy.dufrene94@gmail.com>
2015-09-21 15:24:56 -0700

    don't install brew on non-macs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-echo "Installing bew and node"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install node
+set -e
+
+function getNode() {
+    case $OSTYPE in
+        darwin*)
+            echo "Installing brew and node"
+            command -v brew &> /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            brew install node
+            ;;
+        *)
+            echo "Please install npm and retry"
+            exit 1
+    esac
+}
+
+command -v npm &> /dev/null || getNode
 
 echo "Installing bower"
 npm install -g bower
@@ -10,8 +23,8 @@ npm install -g bower
 echo "Installing gulp"
 npm install -g gulp
 
-echo "Installing despendencies"
+echo "Installing dependencies"
 npm install -g
 bower install && npm install
 
-echo "Now run gulp and you should be good to go\!"
+echo 'Now run gulp and you should be good to go!'
