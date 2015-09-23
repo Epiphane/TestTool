709837536f4f10dd4780f992707ef1e883ed9aa1
Thomas Steinke <exyphnos@gmail.com>
2015-09-23 01:55:41 -0700

    Reconcile heroku vs github

--- main/java/com/teampc/web/server/TestToolApp.java
+++ main/java/com/teampc/web/server/TestToolApp.java
@@ -1,10 +1,12 @@
 package com.teampc.web.server;
 
+
 import com.teampc.web.resource.HelloWorldResource;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.dropwizard.assets.AssetsBundle;
 
 public class TestToolApp extends Application<TestToolConf> {
     public static void main(String... args) throws Exception {
@@ -18,11 +20,12 @@ public class TestToolApp extends Application<TestToolConf> {
 
     @Override
     public void initialize(Bootstrap<TestToolConf> bootstrap) {
-
+        bootstrap.addBundle(new AssetsBundle("/assets", "/", "index.html"));
     }
 
     @Override
     public void run(TestToolConf conf, Environment env) {
+        env.jersey().setUrlPattern("/api/*");
         env.jersey().register(new HelloWorldResource());
     }
 
