bbddc8b1b125d54c758b675c52d961034003e147
Thomas Steinke <exyphnos@gmail.com>
2015-10-21 11:35:45 -0700

    Added some methods to Test

---
+++ specification/src/main/java/com/teampc/test/Key.java
@@ -0,0 +1,11 @@
+package com.teampc.test;
+
+import com.teampc.admin.*;
+import com.teampc.questions.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public abstract class Key extends Submission {
+}
--- specification/src/main/java/com/teampc/test/Test.java
+++ specification/src/main/java/com/teampc/test/Test.java
@@ -1,13 +1,48 @@
 package com.teampc.test;
 
-import com.teampc.questions.*;
+import com.teampc.admin.*;
 import com.teampc.questions.*;
 import java.util.*;
 
+/**
+ * 
+ */
 public abstract class Test {
    
-   private 
+   private Teacher owner;
 
    private ArrayList<Question> questions;
 
+   private boolean published;
+
+   /**
+    * Reorders the question list by moving the question in position
+    * `from` to position `to`.
+    */
+   public abstract void moveQuestion(int from, int to);
+
+   /**
+    * Return the list of questions.
+    */
+   public abstract ArrayList<Question> getQuestions();
+
+   /**
+    * Get the owner of this test
+    */
+   public abstract Teacher getOwner();
+
+   /**
+    * Publish this test
+    */
+   public abstract void publish();
+
+   /**
+    * Returns whether or not the test has been published
+    */
+   public abstract boolean isPublished();
+
+   /**
+    * Returns whether or not the test can be taken home.
+    */
+   public abstract boolean isTakeHome();
 }
