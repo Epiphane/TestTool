c11160d3bd1fa0269c25f9aff2caa35dee325b5d
David Ellison <davidellison341@gmail.com>
2015-10-21 23:45:21 -0700

    David's ms2 eval, some minor model changes, and two of Zach's models

---
+++ specification/src/main/java/com/teampc/testtaking/CodeQuestionResponse.java
@@ -0,0 +1,15 @@
+package com.teampc.testtaking;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * A response to a code question.
+ * @author Zach Arend
+ */
+@Getter
+@Setter
+public abstract class CodeQuestionResponse extends QuestionResponse {
+   //A plaintext version of the student's answer.
+   private String codeAnswer;
+}
