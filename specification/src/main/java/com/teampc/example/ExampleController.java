bb519319f28323f37aae985d5d9791b0bf4e6ca1
adufrene <andy.dufrene94@gmail.com>
2015-10-24 18:32:40 -0700

    add logger

--- specification/src/main/java/com/teampc/example/ExampleController.java
+++ specification/src/main/java/com/teampc/example/ExampleController.java
@@ -6,9 +6,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /* This controller will be set in an fxml file and operates as the intermediary between the view and the data layer */
 /* MVC!!!?!?!? */
 public class ExampleController {
+    private static final Logger log = LoggerFactory.getLogger(ExampleController.class);
+
     @FXML private TableView<ExamplePerson> tableView; // @FXML annotation allows fxml files to access private and protected data
     @FXML private TextField firstNameField;
     @FXML private TextField lastNameField;
@@ -26,6 +31,11 @@ public class ExampleController {
         firstNameField.setText("");
         lastNameField.setText("");
         emailField.setText("");
+    }
+
+    @FXML
+    private void sayHello() {
+        log.info("Hello!");
     }
 
 }
