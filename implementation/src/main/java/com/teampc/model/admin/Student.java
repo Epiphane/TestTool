1c70586384db54ce97e449d30dabff8626250907
Jameson Li <jamesonrli@gmail.com>
2015-10-30 15:14:59 -0700

    spest and fix tabs

--- implementation/src/main/java/com/teampc/model/admin/Student.java
+++ implementation/src/main/java/com/teampc/model/admin/Student.java
@@ -5,38 +5,70 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 
 import com.teampc.model.admin.course.*;
+
 import java.util.List;
 
 /**
- *
  * This class contains information that identifies a Student.
- *
+ * <p>
  * It contains information such as courses a Student is enrolled in.
  */
- @Data
- @EqualsAndHashCode(callSuper=true)
+@Data
+@EqualsAndHashCode(callSuper = true)
 public abstract class Student extends User {
-  /** Student enrolled courses list **/
-  public List<Enrolled> enrolledCourses;
+   /**
+    * Student enrolled courses list
+    **/
+   public List<Enrolled> enrolledCourses;
 
-  /**
-   * Creates a new instance of a Student
-   */
-  public Student(String username, String firstName, String lastName) {
-    super(username, firstName, lastName);
-  }
+   /**
+    * Creates a new instance of a Student
+    */
+   public Student(String username, String firstName, String lastName) {
+      super(username, firstName, lastName);
+   }
 
-  /**
-   * Enrolls a Student into the specified course.
-   * The method will create a new instance of Enrolled and add
-   * this instance to the enrolledCourses list.
-   */
-  public abstract void enrollCourse(Course course);
+   /**
+    * Enrolls a Student into the specified course.
+    * The method will create a new instance of Enrolled and add
+    * this instance to the enrolledCourses list.
+    *
+     pre:
+      //
+      // The enrolledCourses list for this student will not have duplicate courses
+      //
+      !exists (Course cother;
+         enrolledCourses.contains(cother);
+            cother.equals(course))
+     post:
+      //
+      // The resulting enrolledCourses list will contain the course being added
+      //
+      exists(Course cother;
+         enrolledCourses'.contains(cother);
+            cother.equals(course))
+    */
+   public abstract void addCourse(Course course);
 
-  /**
-   * Removes a Student from the specified course.
-   * The method will remove the Enrolled instance
-   * from the enrolledCourses list.
-   */
-  public abstract void removeCourse(Course course);
+   /**
+    * Removes a Student from the specified course.
+    * The method will remove the Enrolled instance
+    * from the enrolledCourses list.
+    *
+     pre:
+      //
+      // The enrolledCourses list must already contain the course being removed
+      //
+      exists(Course cother;
+         enrolledCourses.contains(cother);
+            cother.equals(course))
+    post:
+      //
+      // The resulting enrolledCourses list will not contain the course being removed
+      //
+      !exists(Course cother;
+         enrolledCourses.contains(cother);
+            cother.equals(course))
+    */
+   public abstract void removeCourse(Course course);
 }
--- implementation/src/main/java/com/teampc/model/admin/course/Course.java
+++ implementation/src/main/java/com/teampc/model/admin/course/Course.java
@@ -4,47 +4,68 @@ import lombok.Data;
 import lombok.Builder;
 
 import java.util.List;
+
 import com.teampc.model.admin.*;
 import com.teampc.model.test.Test;
 
 /**
- *
  * This class contains information to uniquely identify a Course.
- *
+ * <p>
  * It contains information such as course title, section, instructor.
  * Methods in this class will be used to modify any related fields.
  */
- @Data
+@Data
 public abstract class Course {
-  /** Teacher assigned to the course **/
-  public Teacher teacher;
-  /** Course title **/
-  public String title;
-  /** Term when the course occurs **/
-  public Term term;
-  /** Year when the course occurs **/
-  public int year;
-  /** Section number to identify the course **/
-  public int section;
-  /**
-   * The list of tests associated with the course
-   */
-  public List<Test> testList;
+   /**
+    * Teacher assigned to the course
+    */
+   public Teacher teacher;
+   /**
+    * Course title
+    */
+   public String title;
+   /**
+    * Term when the course occurs
+    */
+   public Term term;
+   /**
+    * Year when the course occurs
+    */
+   public int year;
+   /**
+    * Section number to identify the course
+    */
+   public int section;
+   /**
+    * The list of tests associated with the course
+    */
+   public List<Test> testList;
 
-  /**
-   * Course will be constructed with the title, term, year, and section
-   */
-  public Course(String title, Term term, int year, int section) {
+   /**
+    * Course will be constructed with the title, term, year, and section
+    */
+   public Course(String title, Term term, int year, int section) {
 
-  }
+   }
 
-  /**
-   * Assigns a teacher to a course.
-   */
-  public abstract void assignTeacher(Teacher teacher);
+   /**
+    * Assigns a teacher to a course.
+    */
+   public abstract void assignTeacher(Teacher teacher);
 
-  /**
-   * Gets a list of students enrolled in this course
-   */
-  public abstract List<Student> getEnrolledStudents();
+   /**
+    * Gets a list of students enrolled in this course
+    *
+     pre:
+      // no preconditions yet.
+     post:
+      //
+      // The return value is a list of students enrolled in this course.
+      // The list is expected to be sorted by the student's name. Student
+      // is expected to have compareTo properly implemented
+      //
+      forall(int i; i>=0 && i<return.size()-1;
+         return.get(i).compareTo(return.get(i+1)) <= 0)
+    */
+   public abstract List<Student> getEnrolledStudents();
 }
