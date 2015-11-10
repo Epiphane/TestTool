d925d156979c1751ed4bd180de16cc28b1bf60a9
Greg Sawers <gsawers1@gmail.com>
2015-11-09 17:50:56 -0800

    Have login page working and hooked to model, just need to add spest

--- implementation/src/main/java/com/teampc/controller/LoginController.java
+++ implementation/src/main/java/com/teampc/controller/LoginController.java
@@ -6,7 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,18 +18,22 @@ import java.io.IOException;
  */
 public class LoginController {
 
+    @FXML private javafx.scene.control.Button loginbutton;
+
     @FXML
     void onLoginClick(ActionEvent event) throws IOException {
         UserSession session = new UserSession(new User());
         session.login();
+        User user = session.getLoggedInUser();
+        System.out.println(user.getDisplayName());
 
-        Stage stage = new Stage();
-        Pane navbar = FXMLLoader.load(getClass().getClassLoader().getResource("navbar.fxml"));
+        Stage stage = (Stage) loginbutton.getScene().getWindow();
+
+
+        ToolBar navbar = FXMLLoader.load(getClass().getClassLoader().getResource("navbar.fxml"));
 
         Scene scene = new Scene(navbar);
         stage.setScene(scene);
         stage.show();
-
-
     }
 }
