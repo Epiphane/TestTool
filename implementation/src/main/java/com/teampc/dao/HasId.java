09fd9a11c87a6d7525cfe13e82c258f1e2ae8136
Andy DuFrene <andy.dufrene@relateiq.com>
2015-12-05 12:51:58 -0800

    add file

---
+++ implementation/src/main/java/com/teampc/dao/HasId.java
@@ -0,0 +1,16 @@
+package com.teampc.dao;
+
+/**
+ * Created by adufrene on 12/5/15.
+ */
+public interface HasId {
+   /**
+    * Returns the id of this object
+    */
+   int getId();
+
+   /**
+    * Sets the given id in the object
+    */
+   void setId(int id);
+}
