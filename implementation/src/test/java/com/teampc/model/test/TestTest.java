50d9c0519e15c37f27da90522e622591ee5c3ab0
Thomas Steinke <exyphnos@gmail.com>
2015-12-09 22:22:07 -0800

    Thomas' unit tests

--- implementation/src/test/java/com/teampc/model/test/TestTest.java
+++ implementation/src/test/java/com/teampc/model/test/TestTest.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.teampc.dao.QuestionDAO;
 import com.teampc.model.automation.TestGrader;
+import com.teampc.model.admin.User;
+import com.teampc.model.testtaking.Submission;
 import com.teampc.model.question.Question;
 import com.teampc.model.testtaking.MatchingQuestionResponse;
 import com.teampc.model.testtaking.MultipleChoiceQuestionResponse;
@@ -16,6 +18,7 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by zarend on 12/9/15.
@@ -24,17 +27,66 @@ public class TestTest {
 
    @Test
    public void testIsOpen() {
-      // TODO
+      Date beforeToday1 = new GregorianCalendar(2013, 10, 11).getTime();     
+      Date beforeToday2 = new GregorianCalendar(2014, 10, 11).getTime();     
+      Date afterToday1 = new GregorianCalendar(2016, 12, 12).getTime();        
+      Date afterToday2 = new GregorianCalendar(2017, 12, 12).getTime();        
+      com.teampc.model.test.Test notPublishedTest = new com.teampc.model.test.Test("Not Published", beforeToday2, afterToday1, "Test Course");
+
+      assertTrue(!notPublishedTest.isOpen());
+
+      com.teampc.model.test.Test notOpenTest = new com.teampc.model.test.Test("Not yet open", afterToday1, afterToday2, "Test Course");
+      notOpenTest.publish();
+      assertTrue(!notOpenTest.isOpen());
+
+      notOpenTest = new com.teampc.model.test.Test("Already closed", beforeToday1, beforeToday2, "Test Course");
+      notOpenTest.publish();
+      assertTrue(!notOpenTest.isOpen());
+
+      com.teampc.model.test.Test openTest = new com.teampc.model.test.Test("Open test!", beforeToday2, afterToday1, "Test Course");
+      openTest.publish();
+      assertTrue(openTest.isOpen());
    }
 
    @Test
    public void testTakeTest() {
-      // TODO
+      com.teampc.model.test.Test test = makeFakeTest();
+
+      String serializedUser = "STUDENT user first last pass";
+      User testStudent = User.fromString(serializedUser);
+
+      Submission testSubmission = test.takeTest(testStudent);
+
+      assertTrue(testSubmission.getTaker().equals(testStudent));      
+      assertTrue(testSubmission.getTest().equals(test));      
+      assertTrue(testSubmission.getTest().equals(test));      
+      assertTrue(!testSubmission.isComplete());      
+      assertTrue(testSubmission.getResponses().size() == test.getQuestions().size());    
+
+      // Initially null for all responses
+      for (int i = 0; i < test.getQuestions().size(); i ++) {
+         assertTrue(testSubmission.getResponses().get(i) == null);
+      }  
    }
 
    @Test
    public void testRemoveQuestion() {
-      // TODO
+      com.teampc.model.test.Test test = makeFakeTest();
+      int originalSize = test.getQuestions().size();
+
+      Question<MultipleChoiceQuestionResponse> question1 = test.getQuestions().get(1);
+
+      test.removeQuestion(question1);
+      assertTrue(!test.getQuestions().get(1).equals(question1));
+      assertTrue(test.getQuestions().size() == originalSize - 1);
+      assertTrue(test.getQuestions().indexOf(question1) <= 0);
+
+      List<Question> questions = test.getQuestions();
+      test.removeQuestion(null);
+      assertTrue(test.getQuestions().equals(questions));
+
+      test.removeQuestion(new Question());
+      assertTrue(test.getQuestions().equals(questions));
    }
 
    @Test
