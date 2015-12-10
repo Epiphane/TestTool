086aa8343fd6c67a217d6cfac2bb10ab376f3542
zarend <zacharyarend@gmail.com>
2015-12-09 20:20:11 -0800

    scaffolding for unit testing Test.java

---
+++ implementation/src/test/java/com/teampc/model/test/TestTest.java
@@ -0,0 +1,36 @@
+package com.teampc.model.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by zarend on 12/9/15.
+ */
+public class TestTest {
+
+   @Test
+   public void testIsOpen() {
+      // TODO
+   }
+
+   @Test
+   public void testTakeTest() {
+      // TODO
+   }
+
+   @Test
+   public void testRemoveQuestion() {
+      // TODO
+   }
+
+   @Test
+   public void testMoveQuestionUp() {
+      // TODO
+   }
+
+   @Test
+   public void testMoveQuestionDown() {
+      // TODO
+   }
+}
