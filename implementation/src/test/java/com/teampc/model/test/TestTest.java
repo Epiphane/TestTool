64083aee50a7d0e61470ae5e26b32177e36b7045
zarend <zacharyarend@gmail.com>
2015-12-09 20:33:20 -0800

    add Test for generateKey

--- implementation/src/test/java/com/teampc/model/test/TestTest.java
+++ implementation/src/test/java/com/teampc/model/test/TestTest.java
@@ -1,6 +1,18 @@
 package com.teampc.model.test;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.teampc.dao.QuestionDAO;
+import com.teampc.model.automation.TestGrader;
+import com.teampc.model.question.Question;
+import com.teampc.model.testtaking.MatchingQuestionResponse;
+import com.teampc.model.testtaking.MultipleChoiceQuestionResponse;
+import com.teampc.model.testtaking.ShortAnswerQuestionResponse;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -32,5 +44,60 @@ public class TestTest {
    @Test
    public void testMoveQuestionDown() {
       // TODO
+   }
+
+   @Test
+   public void testGenerateKey() {
+      com.teampc.model.test.Test test = makeFakeTest();
+
+      test.generateKey();
+      TestGrader.gradeTest(test.getKey(), test.getKey());
+      assert(test.getKey().grade == 100.0);
+   }
+
+   private com.teampc.model.test.Test makeFakeTest() {
+      Question<MultipleChoiceQuestionResponse> question1 = new Question<>();
+      question1.setPoints(1);
+      question1.setPrompt("What is the name of the version control tool used in CPE 307?");
+      question1.setType(Question.QuestionType.MULTIPLE_CHOICE);
+
+      MultipleChoiceQuestionResponse mcCorrectAnswer = new MultipleChoiceQuestionResponse(1, Lists.newArrayList("git", "svn", "mercurial", "repo"));
+      question1.setCorrectAnswer(mcCorrectAnswer);
+
+      Question<MatchingQuestionResponse> question2 = new Question<>();
+      question2.setPoints(3);
+      question2.setPrompt("Match the model classes to their corresponding test classes.");
+      question2.setType(Question.QuestionType.MATCHING);
+
+      MatchingQuestionResponse matchingCorrectAnswer = new MatchingQuestionResponse(ImmutableMap.of(
+         "Question.java", "QuestionTest.java",
+         "Test.java", "TestTest.java",
+         "User.java", "UserTest.java"
+      ));
+
+      question2.setCorrectAnswer(matchingCorrectAnswer);
+
+      Question<ShortAnswerQuestionResponse> question3 = new Question<>();
+      question3.setPoints(5);
+      question3.setPrompt("which IDE do we use?");
+      question3.setType(Question.QuestionType.SHORT_ANSWER);
+
+      ShortAnswerQuestionResponse shortAnswerQuestionResponse = new ShortAnswerQuestionResponse("IntelliJ", ShortAnswerQuestionResponse.MatchType.EXACTLY);
+
+      question3.setCorrectAnswer(shortAnswerQuestionResponse);
+
+      List<Question> questionList = Lists.newArrayList(question1, question2, question3);
+
+      QuestionDAO.getInstance().insert(questionList);
+
+      Date userStartDate = new GregorianCalendar().getTime();
+      GregorianCalendar temp = new GregorianCalendar();
+      temp.add(GregorianCalendar.HOUR, 24);
+      Date userEndDate = temp.getTime();
+
+      com.teampc.model.test.Test fakeTest = new com.teampc.model.test.Test("Midterm 1", userStartDate, userEndDate, "CPE 307");
+      fakeTest.setQuestions(questionList);
+
+      return fakeTest;
    }
 }
